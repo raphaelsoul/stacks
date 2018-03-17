065e95ae2020e04e4b8069c12b476c18d23a7c88
Dechen Zhuang <raphaelsoul@163.com>
2018-03-17 14:43:27 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -17,8 +17,8 @@ sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 sudo yum update -y && sudo yum install -y docker-ce git
 
 # avoid docker permission
-# sudo gpasswd -a $USER docker
-sudo gpasswd -a ec2-user docker
+sudo gpasswd -a $USER docker
+# sudo gpasswd -a ec2-user docker
 
 # install docker-compose 
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
