27e2f7a64d30973d9801ac0543184f577d211ddb
Dechen Zhuang <raphaelsoul@163.com>
2018-04-10 18:35:32 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+sudo useradd jenkins
+sudo mkdir -p /home/jenkins/.ssh
+sudo echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3UGoDlb/sNm8xsNC4zNMvw4OkcQ9lwhAlhFJg1TA0NAFaePsVS4j2YwKEdAJLSxOXmmIm5So5qPH1VOVTXaPxT+cesLoaFJ0a1GGGaGEovqy/OPSm7Uvczx9rfliNW2n2IIG/ssw9e+ogryhoB/kOg65X5Ub0/1/SjCELd5mill8lGXoRfPI0GhLF7G+Yo1KwFY7Boy4e7CjCLhAvpN2zZ7Ge1kRUBLrvRnugQmYTdwn94wHiB5WJDDISTWVamvoC3kEIm8BzqfVGHafm1ZtsJWSGb/rAVXsHH2caIxp/Ia8iz7Y/oPa4X1NjwuGrIFSJoSPpdc6exzs14jsLp9uV raphaelsoul@Koishi-no-MBP.local' > /home/jenkins/.ssh/authorized_keys
+
+sudo chmod 700 /home/jenkins/.ssh
+sudo chmod 600 /home/jenkins/.ssh/authorized_keys
+sudo chown -R jenkins:jenkins /home/jenkins/.ssh
+
+
 # docker env shell for rhel on aws
 
 sudo yum -y update
@@ -14,10 +24,11 @@ sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 # see https://github.com/docker/for-linux/issues/21
 # sudo yum-config-manager --enable rhui-rhel-7-server-rhui-extras-rpms
 
-sudo yum update -y && sudo yum install -y docker-ce git
+sudo yum update -y && sudo yum install -y docker-ce git java
 
 # avoid docker permission
 sudo gpasswd -a $USER docker
+sudo gpasswd -a jenkins docker
 # sudo gpasswd -a ec2-user docker
 
 # install docker-compose 
@@ -27,12 +38,3 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 sudo service docker start
 
-sudo useradd jenkins
-sudo mkdir -p /home/jenkins/.ssh
-sudo echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3UGoDlb/sNm8xsNC4zNMvw4OkcQ9lwhAlhFJg1TA0NAFaePsVS4j2YwKEdAJLSxOXmmIm5So5qPH1VOVTXaPxT+cesLoaFJ0a1GGGaGEovqy/OPSm7Uvczx9rfliNW2n2IIG/ssw9e+ogryhoB/kOg65X5Ub0/1/SjCELd5mill8lGXoRfPI0GhLF7G+Yo1KwFY7Boy4e7CjCLhAvpN2zZ7Ge1kRUBLrvRnugQmYTdwn94wHiB5WJDDISTWVamvoC3kEIm8BzqfVGHafm1ZtsJWSGb/rAVXsHH2caIxp/Ia8iz7Y/oPa4X1NjwuGrIFSJoSPpdc6exzs14jsLp9uV raphaelsoul@Koishi-no-MBP.local' > /home/jenkins/.ssh/authorized_keys
-
-sudo chmod 700 /home/jenkins/.ssh
-sudo chmod 600 /home/jenkins/.ssh/*
-sudo chown -R jenkins:jenkins /home/jenkins/.ssh
-sudo gpasswd -a jenkins docker
-sudo yum install -y java
