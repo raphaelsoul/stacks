8612898f540cac8579a9d18a3bca1e6070e18958
Dechen Zhuang <raphaelsoul@163.com>
2018-07-27 03:57:37 +0800

    Create gitlab-runner.sh

---
+++ gitlab-runner.sh
@@ -0,0 +1,7 @@
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | sudo bash
+sudo yum install gitlab-runner
+
+# for debian
+# curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+# sudo apt-get install gitlab-runner
+
