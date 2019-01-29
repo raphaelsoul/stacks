d90ea33a44cf2a2ff2a099d564a788453031264e
Dechen Zhuang <raphaelsoul@163.com>
2019-01-29 14:16:05 +0800

    Update init.linode.sh

--- init.linode.sh
+++ init.linode.sh
@@ -1,12 +1,12 @@
-mkdir -p ~/.ssh
-curl https://raw.githubusercontent.com/raphaelsoul/stacks/master/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+sudo mkdir -p ~/.ssh
+sudo curl https://raw.githubusercontent.com/raphaelsoul/stacks/master/id_rsa.pub >> ~/.ssh/authorized_keys
+sudo chmod 700 ~/.ssh
+sudo chmod 600 ~/.ssh/authorized_keys
 
 # disable passwd login ssh
 sudo grep -q "ChallengeResponseAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*ChallengeResponseAuthentication[[:space:]]yes.*/c\ChallengeResponseAuthentication no" /etc/ssh/sshd_config || echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
 sudo grep -q "^[^#]*PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config || echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-sudo service ssh restart
+sudo service sshd restart
 
 sudo adduser raphaelsoul
 sudo usermod -aG wheel raphaelsoul
