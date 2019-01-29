94193deffd525b50c1be7edcdd71445378147033
Dechen Zhuang <raphaelsoul@163.com>
2019-01-29 13:33:21 +0800

    Update init.linode.sh

--- init.linode.sh
+++ init.linode.sh
@@ -2,3 +2,4 @@ sudo adduser raphaelsoul
 sudo usermod -aG raphaelsoul
 sudo su - raphaelsoul
 mkdir -p ~/.ssh
+curl https://raw.githubusercontent.com/raphaelsoul/stacks/master/id_rsa.pub >> ~/.ssh/authorized_keys
