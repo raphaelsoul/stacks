e4ed781ffbb12bb57a78714394be1b1968af79d9
Dechen Zhuang <raphaelsoul@163.com>
2019-03-31 17:52:01 +0800

    Update spider.sh

--- spider.sh
+++ spider.sh
@@ -2,8 +2,9 @@
 
 # install python
 yum update -y \
-  && yum install -y yum-utils epel-release \
-  && yum-buillddep -y python \
+  && yum install -y yum-utils epel-release
+
+/usr/bin/yum-builddep -y python \
   && curl -O https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz \
   && tar -xf Python-3.7.2.tgz \
   && cd Python-3.7.2 \
@@ -11,17 +12,12 @@ yum update -y \
   && make && make install \
   && echo "alias python='/usr/local/bin/python3'" > /etc/profile.d/python.sh \
   && chmod 755 /etc/profile.d/python.sh \
-  && source /etc/profile.d/python.sh \
+  && source /etc/profile.d/python.sh
 
 # install tor and privoxy
-  && yum install -y tor privoxy \
-
-# configure privoxy
-  && echo 'forward-socks5 / 127.0.0.1:9050 .' >> ./config \
-
-# enable and start services
+yum install -y tor privoxy \
+  && echo 'forward-socks5 / 127.0.0.1:9050 .' >> /etc/privoxy/config \
   && systemctl start tor \
   && systemctl enable tor \
-
   && systemctl start privoxy \
   && systemctl enable privoxy
