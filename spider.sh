3df6da5a56f344ad0e359b681082f30540b95949
Dechen Zhuang <raphaelsoul@163.com>
2019-03-31 17:25:47 +0800

    Update spider.sh

--- spider.sh
+++ spider.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/env bash
 
 # install python
-yum update -y && yum install -y yum-utils
-yum-buillddep -y python
-curl -O https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-
-tar -xf Python-3.7.2.tgz
-cd Python-3.7.2
-./configure
-make && make install
-
-echo "alias python='/usr/local/bin/python3'" > /etc/profile.d/python.sh
-chmod 755 /etc/profile.d/python.sh
-source /etc/profile.d/python.sh
+yum update -y \
+  && yum install -y yum-utils epel-release \
+  && yum-buillddep -y python \
+  && curl -O https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz \
+  && tar -xf Python-3.7.2.tgz \
+  && cd Python-3.7.2 \
+  && ./configure \
+  && make && make install \
+  && echo "alias python='/usr/local/bin/python3'" > /etc/profile.d/python.sh \
+  && chmod 755 /etc/profile.d/python.sh \
+  && source /etc/profile.d/python.sh \
 
 # install tor and privoxy
-yum install -y epel-release
-yum update -y
-yum install -y tor privoxy
+  && yum install -y tor privoxy \
 
 # configure privoxy
-echo 'forward-socks5 / 127.0.0.1:9050 .' >> ./config
+  && echo 'forward-socks5 / 127.0.0.1:9050 .' >> ./config \
 
 # enable and start services
-systemctl start tor
-systemctl enable tor
+  && systemctl start tor \
+  && systemctl enable tor \
 
-systemctl start privoxy
-systemctl enable privoxy
+  && systemctl start privoxy \
+  && systemctl enable privoxy
