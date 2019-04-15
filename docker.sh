61cf8248b8dcfd00129d15357795c1445607efa8
Dechen Zhuang <raphaelsoul@163.com>
2019-04-15 09:35:30 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,7 +12,7 @@ sudo yum -y update
 sudo yum install -y docker-ce
 
 # install docker-compose 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
